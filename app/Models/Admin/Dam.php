6b7a73bbe6ad6bfdd0fb66177353834b04021c01
vitus121289 <vmtusing@gmail.com>
2022-09-26 11:25:05 +0800

    Declared the fillable property of this model, and added its relationship with the calf model.

--- app/Models/Admin/Dam.php
+++ app/Models/Admin/Dam.php
@@ -2,10 +2,19 @@
 
 namespace App\Models\Admin;
 
+use App\Models\Calf;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dam extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'number' => 'required|min:4|max:255'
+    ];
+
+    public function calfs() {
+        return $this->hasMany(Calf::class);
+    }
 }
