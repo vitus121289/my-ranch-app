9fc43eabb47021ae9e4f3de6e5dced5c235eac22
vitus121289 <vmtusing@gmail.com>
2022-09-26 11:21:07 +0800

    Declared the fillable property for the calf model, also added its relationship to the dam model.

--- app/Models/Admin/Calf.php
+++ app/Models/Admin/Calf.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use App\Http\Livewire\Admin\Dam;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Calf extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'number' => 'required|min:4|max:255',
+        'gender' => 'required|min:1|max:1|alpha',
+        'birthdate' => 'required|date'
+    ];
+
+    public function dam() {
+        $this->belongsTo(Dam::class);
+    }
 }
