0bfdea6683c9f83c44c3e50f3f93c327f45e6c47
vitus121289 <vmtusing@gmail.com>
2022-09-30 11:37:24 +0800

    Added an argument that passes the collection of dams from the database.

--- app/Http/Controllers/Admin/DamController.php
+++ app/Http/Controllers/Admin/DamController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Dam;
 use Illuminate\Http\Request;
 
 class DamController extends Controller
 {
     public function index() {
-        return view('admin.dams.index');
+        return view('admin.dams.index', [
+            'dams' => Dam::all()
+        ]);
     }
 }
